30eea8c569d2e95c908bfa731591e1071ae97967
iboalali <alali@live.de>
2014-06-04 22:58:27 +0200

    first push
    The application is complete, but can use more features

---
+++ TBoI_SaveGameParser/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace TBoI_SaveGameParser {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileAccessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(215, 172);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(134, 172);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Times you killed Mom\'s Heart/It Lives";
+            this.toolTip1.SetToolTip(this.label4, "(Mom Kills in Stats)");
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Times you destroyed Poop";
+            this.toolTip1.SetToolTip(this.label5, "(all Types)");
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Times Blood Donation Machine was used";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(12, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Times you killed Mom";
+            this.toolTip1.SetToolTip(this.label7, "(unknown increase)");
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(12, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(197, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Times you entered the Arcade";
+            // 
+            // lbl8
+            // 
+            this.lbl8.Location = new System.Drawing.Point(215, 101);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(75, 17);
+            this.lbl8.TabIndex = 19;
+            this.lbl8.Text = "------";
+            // 
+            // lbl7
+            // 
+            this.lbl7.Location = new System.Drawing.Point(215, 84);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(75, 17);
+            this.lbl7.TabIndex = 18;
+            this.lbl7.Text = "------";
+            // 
+            // lbl6
+            // 
+            this.lbl6.Location = new System.Drawing.Point(215, 67);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(75, 17);
+            this.lbl6.TabIndex = 17;
+            this.lbl6.Text = "------";
+            // 
+            // lbl5
+            // 
+            this.lbl5.Location = new System.Drawing.Point(215, 50);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(75, 17);
+            this.lbl5.TabIndex = 16;
+            this.lbl5.Text = "------";
+            // 
+            // lbl4
+            // 
+            this.lbl4.Location = new System.Drawing.Point(215, 33);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(75, 17);
+            this.lbl4.TabIndex = 15;
+            this.lbl4.Text = "------";
+            // 
+            // label19
+            // 
+            this.label19.Location = new System.Drawing.Point(12, 152);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(197, 17);
+            this.label19.TabIndex = 22;
+            this.label19.Text = "Number of Rocks you destroyed";
+            this.toolTip1.SetToolTip(this.label19, " (or anyone/anything else) ");
+            // 
+            // label20
+            // 
+            this.label20.Location = new System.Drawing.Point(12, 135);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(197, 17);
+            this.label20.TabIndex = 23;
+            this.label20.Text = "Times killed";
+            this.toolTip1.SetToolTip(this.label20, "(Deaths in Stats)");
+            // 
+            // label21
+            // 
+            this.label21.Location = new System.Drawing.Point(12, 118);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(197, 17);
+            this.label21.TabIndex = 24;
+            this.label21.Text = "Bombs placed";
+            this.toolTip1.SetToolTip(this.label21, "Includes Bombs shot/placed by Dr. Fetus, Mr.Mega and Bob\'s Rotten Head");
+            // 
+            // lbl11
+            // 
+            this.lbl11.Location = new System.Drawing.Point(215, 152);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(75, 17);
+            this.lbl11.TabIndex = 27;
+            this.lbl11.Text = "------";
+            // 
+            // lbl10
+            // 
+            this.lbl10.Location = new System.Drawing.Point(215, 135);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(75, 17);
+            this.lbl10.TabIndex = 26;
+            this.lbl10.Text = "------";
+            // 
+            // lbl9
+            // 
+            this.lbl9.Location = new System.Drawing.Point(215, 118);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(75, 17);
+            this.lbl9.TabIndex = 25;
+            this.lbl9.Text = "------";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.Location = new System.Drawing.Point(88, 177);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(40, 17);
+            this.lblCounter.TabIndex = 28;
+            this.lblCounter.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(12, 177);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 17);
+            this.label10.TabIndex = 29;
+            this.label10.Text = "#File Access";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(303, 24);
+            this.menuStrip1.TabIndex = 30;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileAccessToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // fileAccessToolStripMenuItem
+            // 
+            this.fileAccessToolStripMenuItem.CheckOnClick = true;
+            this.fileAccessToolStripMenuItem.Name = "fileAccessToolStripMenuItem";
+            this.fileAccessToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.fileAccessToolStripMenuItem.Text = "# File Access";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 208);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TBoI Save Data Parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileAccessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TBoI_SaveGameParser/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TBoI_SaveGameParser {
+    public partial class Form1 : Form {
+        string ContentOfFile;
+        string driveLetter;
+        string programFilesPath = @"Program Files\Steam";
+        string programFilesX86Path = @"Program Files (x86)\Steam";
+        string gamePath = @"SteamApps\common\The Binding Of Isaac";
+        string fileName = "serial.txt";
+        string completePath;
+        int counter;
+        bool found;
+        private delegate void updateUIDelegate();
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        FileSystemWatcher watcher;
+        public Form1 () {
+            InitializeComponent();
+            counter = 0;
+            found = false;
+            ContentOfFile = string.Empty;
+            watcher = new FileSystemWatcher();
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "serial.txt";
+            watcher.Changed += watcher_Changed;
+            
+
+            this.Icon = global::TBoI_SaveGameParser.Properties.Resources.The_Binding_of_Isaac;
+
+
+            foreach ( var item in allDrives ) {
+                driveLetter = item.Name;
+                programFilesPath = programFilesPath.Insert( 0, driveLetter );
+                programFilesX86Path = programFilesX86Path.Insert( 0, driveLetter );
+
+                if ( !Directory.Exists( programFilesX86Path ) && !Directory.Exists( programFilesPath ) ) {
+
+                    MessageBox.Show( "Locate the file manually", "Steam Not Found" );
+                    return;
+                }
+
+
+                if ( Directory.Exists( programFilesX86Path + "\\" + gamePath ) ) {
+                    watcher.Path = programFilesX86Path + "\\" + gamePath;
+
+                    if ( !File.Exists( programFilesX86Path + "\\" + gamePath + "\\" + fileName ) ) {
+                        MessageBox.Show( "Locate the file manually", "File not found" );
+
+                    }
+
+                    completePath = programFilesX86Path + "\\" + gamePath + "\\" + fileName;
+                    found = true;
+                    break;
+
+                } else if ( Directory.Exists( programFilesPath + "\\" + gamePath ) ) {
+                    watcher.Path = programFilesPath + "\\" + gamePath;
+
+                    if ( !File.Exists( programFilesPath + "\\" + gamePath + "\\" + fileName ) ) {
+                        MessageBox.Show( "Locate the file manually", "File not found" );
+
+                    }
+
+                    completePath = programFilesPath + "\\" + gamePath + "\\" + fileName;
+                    found = true;
+                    break;
+
+                } else {
+                    MessageBox.Show( "Locate the file manually", "Game Not Installed via Steam" );
+                    return;
+
+                }
+            }
+
+            if ( !found ) {
+                foreach ( var item in allDrives ) {
+
+
+
+                }
+
+            }
+
+            using ( StreamReader sr = new StreamReader( completePath ) ) {
+                ContentOfFile = sr.ReadToEnd();
+                sr.Close();
+
+            }
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        void watcher_Changed ( object sender, FileSystemEventArgs e ) {
+
+            while ( true ) {
+                try {
+                    using ( StreamReader sr = new StreamReader( completePath ) ) {
+                        ContentOfFile = sr.ReadToEnd();
+                        sr.Close();
+
+                    }
+                    break;
+                } catch ( IOException ) {
+
+                    continue;
+                } catch {
+
+                    MessageBox.Show( "Something went wrong", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    Environment.Exit( 1 );
+                }
+            }
+            
+            ParseContent();
+            updateCounter();
+
+        }
+
+        private void updateCounter () {
+
+            if ( lblCounter.InvokeRequired ) {
+                lblCounter.Invoke( new updateUIDelegate( updateCounter ) );
+
+            } else {
+
+                lblCounter.Text = ( ++counter / 2 ).ToString();
+
+
+            }
+
+
+        }
+
+        private void btnExit_Click ( object sender, EventArgs e ) {
+            Environment.Exit( Environment.ExitCode );
+        }
+
+        private void btnOpenFile_Click ( object sender, EventArgs e ) {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            switch ( ofd.ShowDialog() ) {
+                case DialogResult.OK:
+                    using ( StreamReader sr = new StreamReader( ofd.FileName ) ) {
+                        ContentOfFile = sr.ReadToEnd();
+                        completePath = ofd.FileName;
+                        sr.Close();
+
+                    }
+
+                    ParseContent();
+
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private void Form1_Load ( object sender, EventArgs e ) {
+            ParseContent();
+
+        }
+
+
+        private void ParseContent () {
+            if ( ContentOfFile == string.Empty ) {
+                return;
+            }
+            
+            string[] parts = ContentOfFile.Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries );
+
+            //lbl1.Text = parts[1];
+            //lbl2.Text = parts[2] == "1" ? "true" : "false";
+            //lbl3.Text = parts[3];
+            lbl4.Text = parts[4];
+            lbl5.Text = parts[5];
+            lbl6.Text = parts[6];
+            lbl7.Text = parts[7];
+            lbl8.Text = parts[8];
+            lbl9.Text = parts[9];
+            lbl10.Text = parts[10];
+            lbl11.Text = parts[11];
+
+        }
+        
+
+    }
+}
